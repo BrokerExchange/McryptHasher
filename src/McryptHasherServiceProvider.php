3b0a31c68ba31faf095309c67f7a95f188db2d58
Sean Wahlstrom <sean@brokerbin.com>
2023-01-25 09:15:31 -0600

    Update McryptHasherServiceProvider.php
    Auto merge did not correctly merge this previous code push.

--- src/McryptHasherServiceProvider.php
+++ src/McryptHasherServiceProvider.php
@@ -4,12 +4,14 @@
  * User: bmix
  * Date: 10/25/17
  * Time: 10:22 AM
+ *
  */
 
 namespace McryptHasher;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 /**
  * Class McryptHasherServiceProvider
@@ -17,6 +19,24 @@ use Illuminate\Support\ServiceProvider;
  */
 class McryptHasherServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishes([
+            __DIR__.'/../config/mcrypt.php' => config_path('mcrypt.php')
+        ], 'mcrypt');
+    }
 
     /**
      * Bootstrap any application services.
@@ -26,11 +46,19 @@ class McryptHasherServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/mcrypt.php', 'app.mcrypt'
+            __DIR__.'/../config/mcrypt.php', 'mcrypt.key'
         );
 
-        Hash::extend('mcrypt', static function () {
-            return new McryptHasher();
+        Hash::extend('mcrypt', static function(){
+
+            if (Str::startsWith($key = config('mcrypt.key'), 'base64:')) {
+                $key = base64_decode(substr($key, 7));
+            }
+
+            return new McryptHasher($key);
+
         });
+
     }
+
 }
