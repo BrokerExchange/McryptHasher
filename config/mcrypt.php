202a20e44273b5b17b3126a253da621e8d7d9482
Sean Wahlstrom <sean@brokerbin.com>
2023-01-25 09:17:31 -0600

    Update mcrypt.php
    Auto merge did not correctly merge the last update to this file

--- config/mcrypt.php
+++ config/mcrypt.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mcrypt Options
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the configuration options that should be used when
+    | passwords are hashed using the Mcrypt algorithm.
+    |
+    */
+
     'key' => env('MCRYPT_KEY',env('APP_KEY')),
 ];
